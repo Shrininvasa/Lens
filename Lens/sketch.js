5b343df2912c5dfd4d2f93e3e9af065449c10247
Shrininvasa <33095423+Shrininvasa@users.noreply.github.com>
2019-08-01 19:44:45 +0530

    Add files via upload

--- Lens/sketch.js
+++ Lens/sketch.js
@@ -7,24 +7,44 @@ let v = optical_centre + (1 / ((1 / fl) + (1 / (u * -1)))) // Absolute
 let allowObj_inf = false;
 let speed = 2.5;
 let img_x, img_y1, img_y2, img_height;
-let left_clicked = false;
-let right_clicked = false;
-
+let slider, times, psv, pox;
 
 function setup() {
   createCanvas(800, 600);
-  count = 0;
+  slider = createSlider(17.5, 320, obj.x, 2.5);
+  slider.position(17.5, height + 10);
+  slider.style('width', '302.5px')
+  times = 0;
+  psv = slider.value();
+  pox = obj.x;
+  createP("Move the above slider to move the object").position(25, height + 20);
+  checkbox = createCheckbox('Use Mouse/Touch', false).position(330, height + 13.5);
+  //frameRate(120);
 }
 
 function roundTo(n, d) {
   if (n == floor(n)) return n;
-  num = floor(n);
-  diff = n % (10 ** -d);
-  r_num = n - diff
+  // num = floor(n);
+  // diff = n % (10 ** -d);
+  // r_num = n - diff
+  r_num = n;
   r_num *= 10 ** d;
   r_num = floor(r_num);
   r_num /= 10 ** d;
   return r_num;
+}
+
+function sliderSet() {
+  let pSliderVal = psv;
+  let pObj_x = pox;
+
+  if (times > 0) {
+    psv = slider.value();
+    pox = obj.x;
+    if (pSliderVal != slider.value()) obj.x = slider.value();
+    else if (pObj_x != obj.x) slider.elt.value = obj.x;
+  }
+  times += 1;
 }
 
 function calcDist() {
@@ -33,7 +53,8 @@ function calcDist() {
 
   img_height = obj.height * (v - optical_centre) / u;
   img_x = v;
-  img_y1 = 300;
+  if (img_height >= 0) img_y1 = 298;
+  else img_y1 = 302;
   img_y2 = img_y1 - img_height;
 }
 
@@ -46,9 +67,26 @@ function drawData() {
   text(("v :  " + (roundTo((v - optical_centre), 2))), width - 260, 70)
   text(("u :  " + u), width - 260, 100)
   text(("h :  " + obj.height), width - 125, 40)
-  text(("h' :  " + roundTo(img_height, 2)), width - 125, 70)
+  text(("h' :  " + roundTo(img_height, 2)), width - 125, 70);
+
   textSize(40);
-  text("Lens Simulation", 25, 50)
+  text("Lens Simulation", 22, 50);
+  strokeWeight(0.001);
+  textSize(20);
+  fill(255, 215);
+  text("Created By: Sanjay Bhat", width - 225, height - 7.5);
+
+  textSize(12.5);
+  strokeWeight(0.01);
+  text("F", (f * -1) - 4.75, pa.y1 + 17.5);
+  text("2F", (f * -1) - fl - 9, pa.y1 + 17.5);
+  text("F", (optical_centre + fl) - 4.75, pa.y1 + 17.5);
+  text("2F", (optical_centre + 2 * fl) - 9, pa.y1 + 17.5);
+  textSize(8);
+  text("1", (f * -1) + 1.2, pa.y1 + 20);
+  text("1", (f * -1) - fl + 4, pa.y1 + 20);
+  text("2", (optical_centre + fl) + 1.5, pa.y1 + 20);
+  text("2", (optical_centre + 2 * fl) + 4.5, pa.y1 + 20);
 }
 
 function drawPA() {
@@ -99,48 +137,22 @@ function move() {
   if (keyIsDown(LEFT_ARROW)) {
     obj.x -= speed
   }
+  if (mouseY <= height && mouseX <= width && checkbox.checked() == true) obj.x = mouseX + 0.5;
   if (!allowObj_inf) {
     obj.x = constrain(obj.x, 17.5, 320);
-  }
-	 else {
+  } else {
     obj.x = constrain(obj.x, -Infinity, 320);
   }
 
-  if (mouseIsPressed) {
-    if (left_clicked) {
-      obj.x += speed
-    }
-    if (right_clicked) {
-      obj.x -= speed
-    }
-  }
-  // if (showObj_x) console.log(obj.x);
-  //img_x = constrain(img_x, optical_centre, width)
-
-}
-
-function change(a){
-  if (a == 1){
-    left_clicked = true;
-    right_clicked = false;
-  }
-  if (a == -1) {
-    right_clicked = true;
-    left_clicked = false;
-  }
-  if (a == 0) {
-    right_clicked = false;
-    left_clicked = false;
-  }
 }
 
 function setSpeed() {
   if (allowObj_inf) {
+    if (count < 1) console.log("Speed also set to 600 from" + speed);
     speed = 600;
-    if (count < 1) console.log("Speed also set to " + speed);
     count++;
   } else {
-    speed = 2.5
+    speed = 2.5;
   }
 }
 
@@ -156,6 +168,5 @@ function draw() {
   drawImg();
   drawData()
   setSpeed();
-  //drawImg(img_x, img_y1, img_x, img_y2);
-  //console.log(left_clicked);
+  sliderSet();
 }
